f33167767e2fcb966fe067ff39bfab8b3f8eb535
Ivan Domrachev <i_domrachev@kaist.ac.kr>
2025-10-27 14:12:29 +0900

    feat: panda rohand init

---
+++ source/isaaclab_assets/isaaclab_assets/robots/panda_rohand.py
@@ -0,0 +1,97 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Configuration for the Franka Emika Panda arm equipped with the RoHand gripper.
+
+The following configurations are available:
+
+* :obj:`PANDA_ROHAND_CFG`: Franka Panda arm with six independently actuated RoHand fingers.
+"""
+
+import isaaclab.sim as sim_utils
+from isaaclab.actuators import ImplicitActuatorCfg
+from isaaclab.assets.articulation import ArticulationCfg
+from isaaclab_assets import ISAACLAB_ASSETS_DATA_DIR
+
+
+PANDA_ROHAND_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=f"{ISAACLAB_ASSETS_DATA_DIR}/robots/panda_rohand.usd",
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=True,
+            max_depenetration_velocity=5.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=False,
+            solver_position_iteration_count=8,
+            solver_velocity_iteration_count=0,
+        ),
+        # joint_drive_props=sim_utils.JointDrivePropertiesCfg(drive_type="force"),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.0),
+        rot=(0.0, 0.0, 0.0, 1.0),
+        joint_pos={
+            # Franka arm joints (mirrors FRANKA_PANDA_CFG default)
+            "panda_joint1": 0.0,
+            "panda_joint2": -0.569,
+            "panda_joint3": 0.0,
+            "panda_joint4": -2.210,
+            "panda_joint5": 0.0,
+            "panda_joint6": 3.037,
+            "panda_joint7": 0.741,
+            # RoHand actuated joints (fully open span)
+            "th_root_link": 0.0,
+            "th_slider_link": 0.0,
+            "if_slider_link": 0.0,
+            "mf_slider_link": 0.0,
+            "rf_slider_link": 0.0,
+            "lf_slider_link": 0.0,
+        },
+    ),
+    actuators={
+        "panda_shoulder": ImplicitActuatorCfg(
+            joint_names_expr=["panda_joint[1-4]"],
+            effort_limit_sim=87.0,
+            stiffness=400,
+            damping=80,
+        ),
+        "panda_forearm": ImplicitActuatorCfg(
+            joint_names_expr=["panda_joint[5-7]"],
+            effort_limit_sim=12.0,
+            stiffness=400,
+            damping=80,
+        ),
+        "rohand": ImplicitActuatorCfg(
+            joint_names_expr=[
+                "th_root_link",
+                "(th|if|mf|rf|lf)_slider_link",
+            ],
+            effort_limit_sim={
+                "th_root_link": 40.0,
+                "(th|if|mf|rf|lf)_slider_link": 40.0,
+            },
+            stiffness={
+                "th_root_link": 0.06814,
+                "(th|if|mf|rf|lf)_slider_link": 800.0,
+            },
+            damping={
+                "th_root_link": 0.00003,
+                "(th|if|mf|rf|lf)_slider_link": 60.0,
+            },
+            velocity_limit_sim={
+                "th_root_link": 1000.0,
+                "(th|if|mf|rf|lf)_slider_link": 1000.0,
+            },
+            
+        #     friction={
+        #         "th_root_link": 0.02,
+        #         "(th|if|mf|rf|lf)_slider_link": 0.01,
+        #     },
+        ),
+    },
+    soft_joint_pos_limit_factor=1.0,
+)
+"""Configuration of Franka Panda arm with RoHand gripper."""
+
---
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/dexsuite/config/panda_rohand/dexsuite_panda_rohand_env_cfg.py
@@ -0,0 +1,182 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from isaaclab_assets.robots import PANDA_ROHAND_CFG
+
+from isaaclab.managers import ObservationTermCfg as ObsTerm
+from isaaclab.managers import RewardTermCfg as RewTerm
+from isaaclab.managers import SceneEntityCfg
+from isaaclab.managers import EventTermCfg as EventTerm
+from isaaclab.sensors import ContactSensorCfg
+from isaaclab.utils import configclass
+
+from ... import dexsuite_env_cfg as dexsuite
+from ... import mdp
+
+
+@configclass
+class PandaRoHandRelJointPosActionCfg:
+    action = mdp.RelativeJointPositionActionCfg(
+        asset_name="robot",
+        joint_names=[".*"],
+        scale={
+            "panda_joint1": 0.1,
+            "panda_joint2": 0.1,
+            "panda_joint3": 0.1,
+            "panda_joint4": 0.1,
+            "panda_joint5": 0.1,
+            "panda_joint6": 0.1,
+            "panda_joint7": 0.1,
+            "th_root_link": 0.1,
+            "th_slider_link": 0.001,
+            "(if|mf|rf|lf)_slider_link": 0.001,
+        },
+    )
+
+
+@configclass
+class PandaRoHandReorientRewardCfg(dexsuite.RewardsCfg):
+    good_finger_contact = RewTerm(
+        func=mdp.contacts,
+        weight=2.0,
+        params={
+            "threshold": 0.2,
+        },
+    )
+    any_finger_contact = RewTerm(
+        func=mdp.any_contact,
+        weight=1.0,
+        params={
+            "threshold": 0.2,
+        },
+    )
+    table_contact_penalty = RewTerm(
+        func=mdp.table_contact_penalty,
+        weight=-0.5,
+        params={
+            "table_contact_name": "table_s",
+            "threshold": 0.2,
+        },
+    )
+    object_upward_motion = RewTerm(
+        func=mdp.object_upward_velocity_bonus,
+        weight=0.5,
+        params={
+            "std": 0.2,
+            "threshold": 0.2,
+            "thumb_contact_name": "th_slider_link",
+        },
+    )
+
+
+@configclass
+class UR10TessoloEventCfg(dexsuite.EventCfg):
+    fix_robot_joints = EventTerm(
+        func=mdp.reset_joints_by_offset,
+        mode="reset",
+        params={
+            "asset_cfg": SceneEntityCfg("robot", joint_names=["panda_joint2", "(th|if|mf|rf|lf)_slider_link"]),
+            "position_range": [0.0, 0.0],
+            "velocity_range": [0.0, 0.0],
+        },
+    )
+    # reset_robot_elbow2_joint = EventTerm(
+    #     func=mdp.reset_joints_by_offset,
+    #     mode="reset",
+    #     params={
+    #         "asset_cfg": SceneEntityCfg("robot", joint_names="panda_joint3"),
+    #         "position_range": [0.0, 0.0],
+    #         "velocity_range": [0.0, 0.0],
+    #     },
+    # )
+
+    
+@configclass
+class PandaRoHandMixinCfg:
+    rewards: PandaRoHandReorientRewardCfg = PandaRoHandReorientRewardCfg()
+    actions: PandaRoHandRelJointPosActionCfg = PandaRoHandRelJointPosActionCfg()
+    events: UR10TessoloEventCfg = UR10TessoloEventCfg()
+
+    def __post_init__(self: dexsuite.DexsuiteReorientEnvCfg):
+        super().__post_init__()
+        self.scene.robot = PANDA_ROHAND_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+        self.commands.object_pose.body_name = "base_link"
+
+        # The RoHand palm attaches under panda_link7, so align the command sampling to the table frame.
+        self.scene.table.spawn.activate_contact_sensors = True
+
+        thumb_contact_name = "th_distal_link"
+        tip_contact_names = ["if_distal_link", "mf_distal_link", "rf_distal_link", "lf_distal_link"]
+
+        finger_body_names = tip_contact_names + [thumb_contact_name]
+        for link_name in finger_body_names:
+            setattr(
+                self.scene,
+                f"{link_name}_object_s",
+                ContactSensorCfg(
+                    prim_path="{ENV_REGEX_NS}/Robot/franka/rohand_left_flattened/" + link_name,
+                    filter_prim_paths_expr=["{ENV_REGEX_NS}/Object"],
+                ),
+            )
+
+        self.scene.table_s = ContactSensorCfg(
+            prim_path="{ENV_REGEX_NS}/table",
+            filter_prim_paths_expr=["{ENV_REGEX_NS}/Object"],
+        )
+
+        self.observations.proprio.contact = ObsTerm(
+            func=mdp.fingers_contact_force_b,
+            params={"contact_sensor_names": [f"{link}_object_s" for link in finger_body_names]},
+            clip=(-20.0, 20.0),
+        )
+        self.observations.proprio.hand_tips_state_b.params["body_asset_cfg"].body_names = ["base_link"] + finger_body_names
+
+        self.rewards.fingers_to_object.params["asset_cfg"] = SceneEntityCfg(
+            "robot", body_names=["base_link"] + finger_body_names
+        )
+        self.events.reset_robot_wrist_joint.params["asset_cfg"] = SceneEntityCfg(
+            "robot", joint_names=["panda_joint7"]
+        )
+
+        self.rewards.position_tracking.params["thumb_contact_name"] = thumb_contact_name
+        self.rewards.position_tracking.params["tip_contact_names"] = tip_contact_names
+
+        if self.rewards.orientation_tracking:
+            self.rewards.orientation_tracking.params["thumb_contact_name"] = thumb_contact_name
+            self.rewards.orientation_tracking.params["tip_contact_names"] = tip_contact_names
+
+        self.rewards.success.params["thumb_contact_name"] = thumb_contact_name
+        self.rewards.success.params["tip_contact_names"] = tip_contact_names
+
+        self.rewards.good_finger_contact.params["thumb_contact_name"] = thumb_contact_name
+        self.rewards.good_finger_contact.params["tip_contact_names"] = tip_contact_names
+
+        self.rewards.object_upward_motion.params["thumb_contact_name"] = thumb_contact_name
+        self.rewards.object_upward_motion.params["tip_contact_names"] = tip_contact_names
+
+        self.rewards.table_contact_penalty.params["thumb_asset_cfg"] = thumb_contact_name
+        self.rewards.table_contact_penalty.params["tip_asset_cfg"] = tip_contact_names
+
+        self.rewards.any_finger_contact.params["contact_names"] = finger_body_names
+
+
+@configclass
+class DexsuitePandaRoHandReorientEnvCfg(PandaRoHandMixinCfg, dexsuite.DexsuiteReorientEnvCfg):
+    pass
+
+
+@configclass
+class DexsuitePandaRoHandReorientEnvCfg_PLAY(PandaRoHandMixinCfg, dexsuite.DexsuiteReorientEnvCfg_PLAY):
+    pass
+
+
+@configclass
+class DexsuitePandaRoHandLiftEnvCfg(PandaRoHandMixinCfg, dexsuite.DexsuiteLiftEnvCfg):
+    pass
+
+
+@configclass
+class DexsuitePandaRoHandLiftEnvCfg_PLAY(PandaRoHandMixinCfg, dexsuite.DexsuiteLiftEnvCfg_PLAY):
+    pass

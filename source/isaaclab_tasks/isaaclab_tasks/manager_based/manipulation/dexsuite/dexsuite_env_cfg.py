6c84db20ad910b3646cab1169304a98f71069db2
Ivan Domrachev <domrachev03@mail.ru>
2025-11-27 18:47:44 +0900

    feat: working version

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/dexsuite/dexsuite_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/dexsuite/dexsuite_env_cfg.py
@@ -196,8 +196,8 @@ class EventCfg:
         mode="startup",
         params={
             "asset_cfg": SceneEntityCfg("robot", body_names=".*"),
-            "static_friction_range": [0.5, 1.0],
-            "dynamic_friction_range": [0.5, 1.0],
+            "static_friction_range": [1.0, 1.0],
+            "dynamic_friction_range": [1.0, 1.0],
             "restitution_range": [0.0, 0.0],
             "num_buckets": 250,
         },
@@ -208,8 +208,8 @@ class EventCfg:
         mode="startup",
         params={
             "asset_cfg": SceneEntityCfg("object", body_names=".*"),
-            "static_friction_range": [0.5, 1.0],
-            "dynamic_friction_range": [0.5, 1.0],
+            "static_friction_range": [1.0, 1.0],
+            "dynamic_friction_range": [1.0, 1.0],
             "restitution_range": [0.0, 0.0],
             "num_buckets": 250,
         },

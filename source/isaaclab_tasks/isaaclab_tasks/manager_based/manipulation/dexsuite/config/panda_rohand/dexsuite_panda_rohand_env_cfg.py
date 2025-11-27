046a1c091fa19685141a36e8fb12e0d237cb2b0c
Ivan Domrachev <i_domrachev@kaist.ac.kr>
2025-11-27 14:39:20 +0900

    feat: update model & revert some old changes

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/dexsuite/config/panda_rohand/dexsuite_panda_rohand_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/dexsuite/config/panda_rohand/dexsuite_panda_rohand_env_cfg.py
@@ -37,7 +37,7 @@ class PandaRoHandRelJointPosActionCfg:
 class PandaRoHandReorientRewardCfg(dexsuite.RewardsCfg):
     good_finger_contact = RewTerm(
         func=mdp.contacts,
-        weight=3.0,
+        weight=2.0,
         params={
             "threshold": 0.2,
         },
@@ -128,10 +128,10 @@ class PandaRoHandMixinCfg:
             params={"contact_sensor_names": [f"{link}_object_s" for link in finger_body_names]},
             clip=(-20.0, 20.0),
         )
-        self.observations.proprio.hand_tips_state_b.params["body_asset_cfg"].body_names = finger_body_names
+        self.observations.proprio.hand_tips_state_b.params["body_asset_cfg"].body_names = all_body_names
 
         self.rewards.fingers_to_object.params["asset_cfg"] = SceneEntityCfg(
-            "robot", body_names=finger_body_names
+            "robot", body_names=all_body_names
         )
         self.events.reset_robot_wrist_joint.params["asset_cfg"] = SceneEntityCfg(
             "robot", joint_names=["panda_joint7"]
